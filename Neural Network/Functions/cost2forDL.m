947262d2229eea413a3c4403774dec9bcd6b90b9
Xinyu Luo <47040604+RealXinyuLuo@users.noreply.github.com>
2022-01-28 20:31:57 +0000

    Update cost2forDL.m
    added comments for higher readability

--- Neural Network/Functions/cost2forDL.m
+++ Neural Network/Functions/cost2forDL.m
@@ -1,13 +1,16 @@
-% Cost function for 2-coefficient filter - Deep Leaning 
-% Inputs:
-% diff: element-wise difference between input 1d signal and reconstructed signal 
-% LoD: low-pass decomposition filter 
-% LoR: low-pass reconstruction filter 
-% T: Truth, actual input image 
+
 
 function cost_vector = cost2forDL(diff,LoD,LoR,T)
-cost_vector = zeros(4,1);
-diff = diff';   %matlab transpose pg21 ish
+    % Cost function for 2-coefficient filter - Deep Leaning 
+    % Inputs:
+    %       diff: element-wise difference between input 1d signal and reconstructed signal 
+    %       LoD: low-pass decomposition filter 
+    %       LoR: low-pass reconstruction filter 
+    %       T: Truth, actual input image 
+
+    cost_vector = zeros(4,1);
+    diff = diff';   %matlab transpose pg21 ish
+    
     for k = 1 : length(diff) -1
         if rem(k,2) == 1  % if k is an odd number 
             % maths in report
