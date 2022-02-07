41ec3e4c568ffafaa885a16d65dee22c26ea2b83
Xinyu Luo <47040604+RealXinyuLuo@users.noreply.github.com>
2022-02-07 20:33:02 +0000

    Update train_network.m

--- Neural Network/train_network.m
+++ Neural Network/train_network.m
@@ -1,20 +1,18 @@
-samples_number = 12800;     % The number of samples in the training set 
+training_samples = 12800;
 sample_length = 32;
-SNR = 30;     
 filternumber = 2; %2 for 2 coefficient network, 4 for 4 coefficient network
+SNR = 30;
 bits = 3;
 
+
 %%  Generating Target Training data 
-target_signals = cell(samples_number,1);
-quantized_signals = cell(samples_number,1);
-
-
-%images = single_type_images(training_samples, sample_length,SNR,type)
+target_signals = cell(training_samples,1);
+quantized_signals = cell(training_samples,1);
 images = mixedimages(12800, sample_length,SNR);
 
-%% Quantization
 
-for i = 1 : samples_number % the number of columns in training data 
+%% Quantization
+for i = 1 : training_samples % the number of columns in training data 
     target_signals{i} = images(:,i); % Target/ ground truth 
     [quant,quantnoise] = uniformquantization(images(:,i),bits);
     quantized_signals{i} = quant';       % dimention switch to fit my code to Justin's code
@@ -24,14 +22,48 @@ setGlobalImage(quantized_signals); % Set training_et as global to be retrieved i
                               % deep learning pipeline
 
 %% Generating dummy ground truth (As training data is the ground truth
-dummyY = cell(samples_number,1);   % work-around to keep the program happy
-Yrow = size(dummyY,1);
- 
-for i = 1:Yrow
-    a = zeros(filternumber*2,1);  % so that dummyY has the same dimention as output layer 
+dummyY = cell(training_samples,1);   % work-around to keep the program happy
+s = size(dummyY);
+
+for k = 1:s(1)
+    target_images = zeros(filternumber*2,1);  % so that dummyY has the same dimention as output layer 
                                   % we don't need it in this case 
-    dummyY{i} = a;                % filling dummyY with empty elements
+    dummyY{k} = target_images;
 end
+
+% Defining layers
+if filternumber == 2
+    layers = [
+    sequenceInputLayer([sample_length])  %
+    fullyConnectedLayer(10)
+    reluLayer     % ML stuff, read. 
+    fullyConnectedLayer(10)
+    reluLayer
+    fullyConnectedLayer(filternumber*2)    %second last layer has to have the same dimention as the output layer 
+    eluLayer      % ML stuff, read
+    Wavelet2ReconstructionRegressionLayer()];
+
+%   Wavelet2EntropyRegressionLayer()];
+
+else
+    layers = [
+    sequenceInputLayer([sample_length])
+    fullyConnectedLayer(32)
+    reluLayer
+    fullyConnectedLayer(10)
+    reluLayer
+    fullyConnectedLayer(10)
+    reluLayer
+    fullyConnectedLayer(filternumber*2)
+    eluLayer
+    Wavelet4ReconstructionRegressionLayer()];
+end
+
+% Defining training options
+options = trainingOptions('sgdm','InitialLearnRate',0.03,'LearnRateSchedule','piecewise','LearnRateDropPeriod',1,'LearnRateDropFactor',0.2,'Momentum',0.9,'GradientThreshold',1,'L2Regularization',0.1,'MaxEpochs',2,'plots','training-progress');
+
+% main 
+net = trainNetwork(target_signals,dummyY,layers,options);
 
 %% Defining layers
 if filternumber == 2
