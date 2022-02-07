7772d722e0b58497d8505904ca6c8ec9f5a0d866
Xinyu Luo <47040604+RealXinyuLuo@users.noreply.github.com>
2022-02-07 20:34:54 +0000

    Update mixedimages.m
    improved on Justin's original mixed images function

--- Neural Network/Functions/mixedimages.m
+++ Neural Network/Functions/mixedimages.m
@@ -1,94 +1,55 @@
-% Generating random 1d sigansl for analysis in this report 
-% Signal Number: size of the training data set 
-% signal length: size of each training data 
+function images = mixedimages(signal_number, signal_length,SNR)
+%This function generates random 1d signals for analysis in this report
+%   Signal Number: size of the training data set 
+%   Signal Length: size of each training data 
+%   SNR: Signal-to-Noise ratio of Gaussian noise added to the signal 
 
-function images = mixedimages(signal_number, signal_length)
-images = zeros(signal_length,signal_number);
-
-for k = 1 : signal_number
+    images = zeros(signal_length,signal_number);  % Empty matrix storage
     
-    n = randi(5); 
-    
-    %sine wave
-    if n == 1
-       increment = 2*pi/signal_length;  % step size 
-       t = [0:increment:2*pi-increment];
-       amp = randi(10);    % random amplitdude 
-       freq = randi(3);    % random frequency 
-       shift = randi(10);  % random phase 
-
-       signal = amp*sin(freq*t+shift);
-       signal = signal'; 
-       signal = normalize(signal,'range');    %normalise between 0-1
-       images(:,k) = signal;   % store the signal inside the image_store
+    for k = 1 : signal_number
+        n = randi(7); 
+        
+        %sine wave
+        if n == 1
+            signal = randomsinewave(signal_length);
+        end
+        
+        %pulse signal
+        if n == 2
+            signal = randompulsewave(signal_length);
+        end
+        
+        %random noise
+        if n == 3
+            signal = rand(signal_length,1);
+        end
+        
+        %squarewave
+        if n == 4
+            signal = randomsquarewave(signal_length);
+        end
+        
+        %sawtooth
+        if n == 5
+            signal = randomsawtooth(signal_length);
+        end
+        
+        %step
+        if n == 6
+            signal = randomstep(signal_length);
+        end
+        
+        %ramp
+        if n == 7
+            signal = randomramp(signal_length);
+        end
+        
+    signal = awgn(signal,SNR);  % Add Gaussian noise   
+            
+    signal = signal'; 
+    signal = normalize(signal,'range');    % Normalise between 0-1
+    signal = round(signal,4);
+    images(:,k) = signal;   %Store each inside the image_store
     end
-    
-    %pulse signal
-    if n == 2
-        pulse_point = randi(signal_length);
-        images(pulse_point,k) = randi(10);
-    end
-    
-    %random noise
-    if n == 3
-        images(:,k) = rand(signal_length,1);
-    end
-    
-    %squarewave
-    if n == 4
-       increment = 2*pi/signal_length;
-       t = [0:increment:2*pi-increment];
-       amp = randi(5);
-       freq = randi(3);
-       shift = randi(2);
-       signal = amp*square(freq*t+shift);
-       signal = signal';
-       signal = normalize(signal,'range');
-       images(:,k) = signal;
-    end
-    
-    %sawtooth
-    if n == 5
-       increment = 2*pi/signal_length;
-       t = [0:increment:2*pi-increment];
-       amp = randi(5);
-       freq = randi(3);
-       shift = randi(2);
-       signal = amp*sawtooth(freq*t+shift);
-       signal = signal';
-       signal = normalize(signal,'range');
-       images(:,k) = signal;
-    end
-    
-    
-% %     %step
-%     if n == 6
-%         increment = 2/signal_length;
-%        t = (-1:increment:1-increment)';
-%        shift = randn;
-%        shift_vec = ones(numel(t),1).*shift;
-%        amplitude = randn;
-%        unitstep = t>=0;
-%        signal = (unitstep+shift_vec).*amplitude;
-%        images(:,k) = signal;
-%     end
-%     
-%     %ramp
-%     if n == 7
-%         increment = 2/signal_length;
-%        t = (-1:increment:1-increment)';
-%        unitstep = t>=randn;
-%        signal = t.*unitstep;
-%        images(:,k) = signal;
-%     end
-    
-    
-    
-end
-    
-
-
-
-
 
 end
