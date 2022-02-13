e7c8ccee429ec87e7d95a068a9707952335f58fb
Xinyu Luo <47040604+RealXinyuLuo@users.noreply.github.com>
2022-02-13 20:46:45 +0000

    Update train_network.m

--- Neural Network/train_network.m
+++ Neural Network/train_network.m
@@ -1,25 +1,30 @@
+clear
+clc
+
 %%  Independent variables
 training_samples = 12800;
 sample_length = 34;
 filternumber = 2; %2 for 2 coefficient network, 4 for 4 coefficient network
 SNR = 30;
 bits = 3;
+type = 'sine';
+isSNRuniform = true;
+issingletype = false;   
 
 %%  Dependent variables
-nn_input_length = sample_length+1;
+nn_input_length = sample_length+1;  %neural network input includes image and bits
 
-
-%%  Generating data storage
+%%  Generating empty data storage
 target_samples = cell(training_samples,1);
 quantized_samples = cell(training_samples,1);
-feature_vectors  = cell(training_samples,1);
-
+feature_vectors = cell(training_samples,1);
+feature_vector = zeros(nn_input_length,1);
 
 %%  Generating image data
-images = mixedimages(training_samples, sample_length,SNR);
+%images = mixedimages(training_samples, sample_length,SNR);
+images = image_generator(training_samples,sample_length,SNR,type,isSNRuniform,issingletype);
 
 %%  Quantization of image data 
-
 for i = 1 : training_samples           % the number of columns in training data 
     quantized_samples{i} = uniformquantization(target_samples{i},bits);   % Target/ ground truth 
 end
@@ -28,21 +33,16 @@ end
 for i = 1 : training_samples           % the number of columns in training data 
     target_samples{i} = images(:,i);   % Target/ ground truth
 
-    feature_vector = images(:,i);
-    feature_vector(end+1) = bits;
+    feature_vector(1:sample_length) = images(:,i);
+    feature_vector(end) = bits;
 
     feature_vectors{i} = feature_vector;
     
 end
 
-%% Quantization 
-
+%% Defining ground truth 
 setGlobalImage(target_samples);        % Set training_set as global to be retrieved inside 
                                        % deep learning pipeline
-
-%%  Load feature vector 
-% into cell
-
 
 %% Generating dummy response vector
 dummyY = cell(training_samples,1);     % work-around to keep the program happy
@@ -55,25 +55,24 @@ end
 
 responses = dummyY;
 responses_size = size(responses,1);
+
 %%  Defining layers
-
-
 if filternumber == 2
     layers = [
-    sequenceInputLayer([nn_input_length])  
+    sequenceInputLayer(nn_input_length)  
     fullyConnectedLayer(10)
-    reluLayer     % ML stuff, read 
+    reluLayer     
     fullyConnectedLayer(10)
     reluLayer
     fullyConnectedLayer(filternumber*2)    %second last layer has to have the same dimention as the output layer 
-    eluLayer      % ML stuff, read
+    eluLayer      
     Wavelet2ReconstructionRegressionLayer()];
 
 %   Wavelet2EntropyRegressionLayer()];
 
 else
     layers = [
-    sequenceInputLayer([nn_input_length])
+    sequenceInputLayer(nn_input_length)
     fullyConnectedLayer(32)
     reluLayer
     fullyConnectedLayer(10)
@@ -84,6 +83,7 @@ else
     eluLayer
     Wavelet4ReconstructionRegressionLayer()];
 end
+
 %%  Defining training options
 options = trainingOptions('sgdm','InitialLearnRate',0.03,'LearnRateSchedule','piecewise','MiniBatchSize',64,'LearnRateDropPeriod',1,'LearnRateDropFactor',0.2,'Momentum',0.9,'GradientThreshold',1,'L2Regularization',0.1,'MaxEpochs',2,'plots','training-progress');
 %%  main 
