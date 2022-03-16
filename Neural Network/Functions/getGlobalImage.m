de12f0144b7480b184967d7dc01bdc9533c4aecc
Xinyu Luo <47040604+RealXinyuLuo@users.noreply.github.com>
2022-03-16 13:31:27 +0000

    Update getGlobalImage.m

--- Neural Network/Functions/getGlobalImage.m
+++ Neural Network/Functions/getGlobalImage.m
@@ -1,4 +1,6 @@
 % Using a global variable to pass target image into the neural network
+% This is legacy code from Justin's project
+% No longer used in Xinyu's code
 
 function r = getGlobalImage
   global image
