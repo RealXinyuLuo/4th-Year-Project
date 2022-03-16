9b5d2bb4b428c0d4ced124ba634eac789cf7d787
Xinyu Luo <47040604+RealXinyuLuo@users.noreply.github.com>
2022-03-16 13:42:43 +0000

    Update Wavelet4ReconstructionRegressionLayer.m

--- Neural Network/Class/Wavelet4ReconstructionRegressionLayer.m
+++ Neural Network/Class/Wavelet4ReconstructionRegressionLayer.m
@@ -1,101 +1,77 @@
 classdef Wavelet4ReconstructionRegressionLayer < nnet.layer.RegressionLayer
-    % Example custom regression layer with mean-absolute-error loss.
+    properties
+        T
+        bits
+    end
     
     methods
-        function layer = TestRegressionLayer(name)
-            % layer = maeRegressionLayer(name) creates a
-            % mean-absolute-error regression layer and specifies the layer
-            % name.
-			
-            % Set layer name.
-            layer.Name = name;
+        function layer = Wavelet4ReconstructionRegressionLayer(target,bits)
 
-            % Set layer description.
-            layer.Description = 'Output layer for 4 coefficient wavelet reconstruction learning';
+            layer.Name = 'Wavelet4ReconstructionRegressionLayer';
+            layer.Description = 'Output layer for 2 coefficient wavelet entropy reduction learning';
+            layer.T = target;
+            layer.bits = bits;
         end
         
         function loss = forwardLoss(layer, Y, T)
-%             isYsingle = fi(Y);
-%             isYsingle = issingle(isYsingle);
-            
-      %T updates with a new column after each sample in the minibatch,
-                                % So we need to make sure we are using the
-                                % latest column
-            T = double(T);
+
+            T = layer.T;
             Y = double(Y);
-            
-            s = size(Y);
-            minibatch_size = s(2);
-            
+
+            Ycol = size(Y,2);
+            minibatch_size = Ycol;
             loss = 0;
             
             for k = 1 : minibatch_size
-                
+                T_k = cell2mat(T(k,:));
+                [LoD,LoR,HiD,HiR] = make4coeffwavelet(Y(:,k));            
 
-                LoD = Y(1:4,k); % First half of output vector is decomp filter
-                LoR = Y(5:8,k); % Second half is synthesis filter
-                HiD = LoR;
-                HiD(1) = - HiD(1);
-                HiD(3) = - HiD(3);
-                HiR = LoD;
-                HiR(2) = -HiR(2);
-                HiR(4) = -HiR(4);
-
-                T = getGlobalImage;
-                
-                T_k = T(k,:);
-                T_k = cell2mat(T_k);
                 [cA,cD] = dwt(T_k,LoD,HiD);
-                rec = idwt(cA,cD,LoR,HiR); %Inverse DW transform
-                 
+
+                % quantization
+                quantized_cA = uniformquantization(cA,layer.bits)';
+                quantized_cD = uniformquantization(cD,layer.bits)';
+
+                rec = idwt(quantized_cA,quantized_cD,LoR,HiR);       
+
+                %rec = idwt(cA,cD,LoR,HiR);         
                 rec = double(rec);
 
                 loss_individual = immse(rec,T_k);
-                loss = loss + loss_individual;   %Adding up loss for the minibatch
+                loss = loss + loss_individual; 
 
-
-                
             end
-            
-            
-            
+                   
             loss = loss / minibatch_size;
-%             if isYsingle == 1
             loss = single(loss);
-%             end
-            %loss = single(loss);
 
         end
+
         function dLdY = backwardLoss(layer, Y, T)
 
-            T = double(T);
+            T = layer.T;
             Y = double(Y);
             
-            s = size(Y);
-            minibatch_size = s(2);
-            
-%             isYsingle = fi(Y);
-%             isYsingle = issingle(isYsingle);
-            
-            dLdY = zeros(s(1),s(2));
+            Yrow = size(Y,1);
+            Ycol = size(Y,2);
+
+            minibatch_size = Ycol;
+            dLdY = zeros(Yrow,Ycol);
             
             for k = 1 : minibatch_size
+                T_k = cell2mat(T(k,:));             
+                [LoD,LoR,HiD,HiR] = make4coeffwavelet(Y(:,k));     
                 
-                LoD = Y(1:4,k); % First half of output vector is decomp filter
-                LoR = Y(5:8,k); % Second half is synthesis filter
-                
-                HiD = LoR;
-                HiD(1) = - HiD(1);
-                HiD(3) = - HiD(3);
-                HiR = LoD;
-                HiR(2) = -HiR(2);
-                HiR(4) = -HiR(4);
 
-                T = getGlobalImage;
-                
-                T_k = T(k,:);
-                T_k = cell2mat(T_k);
                 [cA,cD] = dwt(T_k,LoD,HiD);
+
+                % quantization
+                quantized_cA = uniformquantization(cA,layer.bits)';
+                quantized_cD = uniformquantization(cD,layer.bits)';
+
+                %rec = idwt(quantized_cA,quantized_cD,LoR,HiR);   
+
+
                 rec = idwt(cA,cD,LoR,HiR);
                 rec = double(rec);
 
@@ -105,15 +81,12 @@ classdef Wavelet4ReconstructionRegressionLayer < nnet.layer.RegressionLayer
 
             end         
             dLdY = dLdY./minibatch_size;
-             
-%             if isYsingle == 1
+
             dLdY = single(dLdY);
-%             end
-%             dLdY = single(dLdY);
+
 
             
         end
-    
-            
-        end
+             
+    end
 end
