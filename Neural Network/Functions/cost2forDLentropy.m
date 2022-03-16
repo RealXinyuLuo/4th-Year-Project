0fb98550e29544c1080ad2aa559daa92c449920d
Xinyu Luo <47040604+RealXinyuLuo@users.noreply.github.com>
2022-03-16 12:36:37 +0000

    Update cost2forDLentropy.m
    This is looking at how different from a black image the latent space is

--- Neural Network/Functions/cost2forDLentropy.m
+++ Neural Network/Functions/cost2forDLentropy.m
@@ -1,17 +1,19 @@
-function cost_vector = cost2forDLentropy(diff,T)
-cost_vector = zeros(4,1);
-diff = diff';
-    for k = 1 : length(diff) -1
+function cost_vector = cost2forDLentropy(cA_diff,cD_diff,LoD,LoR,T)
+    cost_vector = zeros(4,1);
+    cA_diff = cA_diff';
+    cD_diff = cD_diff';
+    
+    for k = 1 : length(cA_diff)
         if rem(k,2) == 1
-            cost_vector(1) = cost_vector(1) + T(k) * diff(k);
-            cost_vector(2) = cost_vector(2) + T(k+1) * diff(k);
-            cost_vector(3) = cost_vector(3) + T(k) * diff(k);
-            cost_vector(4) = cost_vector(4) + T(k+1) * diff(k);
+%             cost_vector(1) = cost_vector(1) + T(k) * cA_diff(k);
+            cost_vector(2) = cost_vector(2);
+            cost_vector(3) = cost_vector(3) + T(k) * cD_diff(k);
+            cost_vector(4) = cost_vector(4);
         else
-            cost_vector(1) = cost_vector(1) + T(k-1) * diff(k);
-            cost_vector(2) = cost_vector(2) + T(k) * diff(k);
-            cost_vector(3) = cost_vector(3) + T(k-1) * diff(k);
-            cost_vector(4) = cost_vector(4) + T(k) * diff(k);
+            cost_vector(1) = cost_vector(1);
+%             cost_vector(2) = cost_vector(2) + T(k) * cA_diff(k-1);
+            cost_vector(3) = cost_vector(3);
+            cost_vector(4) = cost_vector(4) - T(k) * cD_diff(k-1);
         end
     end
         
